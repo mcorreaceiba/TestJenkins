13fba70f6d5068ad2ff0005c64eabb670efa6b2c
CEIBA\moises.correa <moises.correa@ceiba.com.co>
2017-07-21 10:55:12 -0500

    Creación de métodos

--- TestJenkins/src/Model/Person.java
+++ TestJenkins/src/Model/Person.java
@@ -1,7 +1,42 @@
 package Model;
 
+/**
+ * @author moises.correa
+ *
+ */
 public class Person {
-	// Comentario 1.1
-	// Comentario 1.5
-
+	private int id;
+	private String name;
+	private int age;
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param age
+	 */
+	public Person(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
--- TestJenkins/src/main/main.java
+++ TestJenkins/src/main/main.java
@@ -1,10 +1,12 @@
 package main;
+import Model.Person;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub sdasd
-		System.out.println("Hello world");
+		// TODO Auto-generated method
+		Person person1 = new Person(1,"Moisés",25);
+		System.out.println("Hello "+person1.getName().toString());
 	}
 
 }
